920023001c913db7b577418983dbfd7afabd0816
Mike Bannister <notimpossiblemike@gmail.com>
2024-09-01 10:24:33 -0400

    Misc

--- backup-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rsync -a --progress --exclude='*/' /home/mike/ /media/veracrypt2/backups/$(date +%Y%m%d)/
-rsync -a --progress /home/mike/code /media/veracrypt2/backups/$(date +%Y%m%d)/code
-rsync -a --progress /home/mike/.config /media/veracrypt2/backups/$(date +%Y%m%d)/dot-config
-rsync -a --progress /home/mike/.var /media/veracrypt2/backups/$(date +%Y%m%d)/dot-var
-rsync -a --progress /home/mike/.ssh /media/veracrypt2/backups/$(date +%Y%m%d)/dot-ssh
-
-echo "done creating backup."
